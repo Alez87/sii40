28900035e53baa442e7b07ba36c229548a0838d8
alez87 <superalle87@gmail.com>
2022-07-25 18:10:29 +0200

    update scripts

--- demo_zenoh_storage.sh
+++ demo_zenoh_storage.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-demo_db1="example4"
+demo_db1="example10"
 api_port1="8001"
 curl -X PUT -H 'content-type:application/properties' -d "path_expr=/demo/example/**;path_prefix=/demo/example;on_closure=drop_series;db=${demo_db1};create_db" http://localhost:${api_port1}/@/router/local/plugin/storages/backend/influxdb/storage/example
 echo
@@ -9,7 +9,7 @@ echo
 echo "----------------------------------------------------"
 echo
 
-demo_db2=${demo_db1}
+demo_db2="example11"
 api_port2="8002"
 curl -X PUT -H 'content-type:application/properties' -d "path_expr=/demo/example/**;path_prefix=/demo/example;on_closure=drop_series;db=${demo_db2};create_db" http://localhost:${api_port2}/@/router/local/plugin/storages/backend/influxdb/storage/example
 echo
